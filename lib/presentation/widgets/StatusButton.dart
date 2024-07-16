dfc5b176bcfae916e55e9d3b9a9a0d94a6ed9897
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2024-07-16 12:14:48 +0530

    fixed issues

--- lib/presentation/widgets/StatusButton.dart
+++ lib/presentation/widgets/StatusButton.dart
@@ -10,21 +10,18 @@ class StatusButton extends StatelessWidget {
       width: 88,
       height: 32,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        color: status == "Pending" ? const Color(0xFFD44949) : const Color(0xff76FF6A),
+      borderRadius: BorderRadius.circular(4),
+      color: status == "Pending" ? const Color(0xFFD44949) : const Color(0xff76FF6A),
       ),
       padding: const EdgeInsets.all(4),
-      margin: const EdgeInsets.symmetric(horizontal: 10),
+      margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
       child: Center(
-        child: Text(
-          status,
-          style: TextStyle(
-            color: status == "Pending" ? const Color(0xffF6EFDF) : const Color(0xff3D3936),
-            fontSize: 12,
-            fontWeight: FontWeight.w400,
-            fontFamily: 'Poppins',
-          ),
-        ),
+        child: Text(status, style: TextStyle(
+          color: status == "Pending" ? const Color(0xffF6EFDF) : const Color(0xff3D3936),
+          fontSize: 12,
+          fontWeight: FontWeight.w400,
+          fontFamily: 'Poppins'
+        ),),
       ),
     );
   }
